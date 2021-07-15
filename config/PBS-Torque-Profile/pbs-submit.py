651b23503c20ae3a4d3d5e4e7df16bb6228632e3
Graeme Ford <frd.graeme@gmail.com>
2021-07-15 13:20:29 +0200

    Removed module_loads. Snakemake is interfering

--- config/PBS-Torque-Profile/pbs-submit.py
+++ config/PBS-Torque-Profile/pbs-submit.py
@@ -167,10 +167,6 @@ if "resources" in job_properties:
     if "walltime" in resources:
         walltime = "walltime=" + str(resources["walltime"])
 
-    if 'module_loads' in resources:
-        module_loads = "module load " + \
-            resources['module_loads'].join(" ") + ' &&'
-
 if nodes or ppn or mem or walltime:
     resourceparams = " -l \""
 if nodes:
@@ -197,9 +193,9 @@ if "cluster" in job_properties:
         os.makedirs(os.path.dirname(cluster["output"]), exist_ok=True)
         so = " -o " + cluster["output"]
 
-cmd = "{modules} qsub {a}{A}{b}{c}{C}{d}{D}{e}{f}{h}{j}{l}{m}{M}{N}{o}{p}{P}{q}{t}{u}{v}{V}{w}{W}{rp}{dep}{ex}".format(modules=module_loads,
-                                                                                                                       a=atime, A=acc_string, b=pbs_time, c=chkpt, C=pref, d=dd, D=rd, e=se, f=ft, h=hold, j=j, l=resource, m=mail, M=mailuser,
-                                                                                                                       N=jname, o=so, p=priority, P=proxy, q=q, t=ar, u=user, v=ev, V=eall, w=wd, W=add, rp=resourceparams, dep=depend, ex=extras)
+cmd = "qsub {a}{A}{b}{c}{C}{d}{D}{e}{f}{h}{j}{l}{m}{M}{N}{o}{p}{P}{q}{t}{u}{v}{V}{w}{W}{rp}{dep}{ex}".format(modules=module_loads,
+                                                                                                             a=atime, A=acc_string, b=pbs_time, c=chkpt, C=pref, d=dd, D=rd, e=se, f=ft, h=hold, j=j, l=resource, m=mail, M=mailuser,
+                                                                                                             N=jname, o=so, p=priority, P=proxy, q=q, t=ar, u=user, v=ev, V=eall, w=wd, W=add, rp=resourceparams, dep=depend, ex=extras)
 
 try:
     res = subprocess.run(cmd, check=True, shell=True, stdout=subprocess.PIPE)
